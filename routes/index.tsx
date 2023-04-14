2fa3873a69620923421adc7f4f4919886ce59d03
迷渡 <justjavac@gmail.com>
2023-04-14 15:51:01 +0800

    修复首页

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,26 +3,8 @@ import { Handlers, PageProps } from "$fresh/server.ts";
 import { GraphSeriesOption } from "echarts";
 import NotGate from "@/islands/NotGate.tsx";
 import type { Model } from "@/dnn/model.ts";
+import modle from "@/models/basic.json" assert { type: "json" };
 
-export const handler: Handlers<Model | null> = {
-  async GET(_req, ctx) {
-    const modle = await import(`@/models/basic.json`, {
-      assert: { type: "json" },
-    });
-    if (!modle) {
-      return new Response("Modle not found", { status: 404 });
-    }
-    return ctx.render(modle.default as Model);
-  },
-};
-
-export default function Home({ data: model, params }: PageProps<Model | null>) {
-  const nodes: GraphSeriesOption["nodes"] = [];
-  const links: GraphSeriesOption["links"] = [];
-
-  if (model == null) {
-    return "404";
-  }
-
-  return <NotGate model={model} title="与门" />;
+export default function Home() {
+  return <NotGate model={modle as Model} title="一个最小化的神经网络" />;
 }
