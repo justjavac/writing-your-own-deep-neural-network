1ba76bb611f66227cfe52c951e140a40a7183d0b
迷渡 <justjavac@gmail.com>
2023-04-17 16:50:53 +0800

    fix menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -7,7 +7,7 @@ export function Menu({ title, href }: MenuProps) {
   return (
     <div class="align-middle mt-1 flex items-center">
       <a class="cursor-pointer" href={href}>
-        <p class="font-medium text-gray-500 hover:text-gray-900">{title}</p>
+        <p class="font-medium text-gray-500 hover:text-gray-900 mb-0">{title}</p>
       </a>
     </div>
   );
