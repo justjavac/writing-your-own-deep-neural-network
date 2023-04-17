1b8d6458ff663822c0b0103d26b049ff0f19c23b
迷渡 <justjavac@gmail.com>
2023-04-17 12:49:25 +0800

    修正个数

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,9 @@ export function Button({ disabled = false, children, onClick }: ButtonProps) {
       type="button"
       disabled={disabled}
       class={`border focus:outline-none rounded px-4 py-1 text-center ${
-        disabled ? "cursor-not-allowed text-gray-700" : "hover:text-white hover:bg-blue-800 border-blue-700 text-blue-700"
+        disabled
+          ? "cursor-not-allowed text-gray-700"
+          : "hover:text-white hover:bg-blue-800 border-blue-700 text-blue-700"
       }`}
       onClick={onClick}
     >
--- routes/[name].tsx
+++ routes/[name].tsx
@@ -2,17 +2,12 @@ import { PageProps } from "$fresh/server.ts";
 import AndGate from "@/islands/AndGate.tsx";
 import NotGate from "@/islands/NotGate.tsx";
 import Add from "@/islands/Add.tsx";
-import type { Model } from "@/dnn/model.ts";
-import and from "@/models/and_gate.json" assert { type: "json" };
-import or from "@/models/or_gate.json" assert { type: "json" };
-import not from "@/models/not_gate.json" assert { type: "json" };
-import add from "@/models/add.json" assert { type: "json" };
 
-export default function Home({ params }: PageProps<Model | null>) {
+export default function Home({ params }: PageProps) {
   if (params.name === "and_gate") {
     return (
       <AndGate
-        layers={and.map((x) => x.length)}
+        layers={[2, 5, 5, 1]}
         modelName="and_gate"
         title="与门"
       />
@@ -20,7 +15,7 @@ export default function Home({ params }: PageProps<Model | null>) {
   } else if (params.name === "or_gate") {
     return (
       <AndGate
-        layers={or.map((x) => x.length)}
+        layers={[2, 5, 5, 1]}
         modelName="or_gate"
         title="或门"
       />
@@ -28,7 +23,7 @@ export default function Home({ params }: PageProps<Model | null>) {
   } else if (params.name === "not_gate") {
     return (
       <NotGate
-        layers={not.map((x) => x.length)}
+        layers={[1, 5, 5, 1]}
         modelName="not_gate"
         title="非门"
       />
@@ -36,7 +31,7 @@ export default function Home({ params }: PageProps<Model | null>) {
   } else if (params.name === "add") {
     return (
       <Add
-        layers={add.map((x) => x.length)}
+        layers={[10, 5, 5, 10]}
         modelName="add"
         title="十以内加法"
       />
