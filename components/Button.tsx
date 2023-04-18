744ef527b387010dba7e9984ac58ed8918d27d62
迷渡 <justjavac@gmail.com>
2023-04-18 08:43:50 +0800

    调整边距

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,7 +9,7 @@ export function Button({ disabled = false, children, onClick }: ButtonProps) {
     <button
       type="button"
       disabled={disabled}
-      class={`border focus:outline-none rounded px-4 py-1 text-center ${
+      class={`border focus:outline-none rounded px-3 py-1 text-center ${
         disabled
           ? "cursor-not-allowed text-gray-700"
           : "hover:text-white hover:bg-blue-800 border-blue-700 text-blue-700"
